d3e11b5eb147f9a5ecd96a4d5b1cc72d693448ae
HADJIAHMED2219 <ahmed.hadji2219@gmail.com>
2025-06-14 21:59:59 +0100

    app.py

--- app.py
+++ app.py
@@ -25,5 +25,5 @@ def home():
     return "Bienvenue sur l'API Proxy ESP32 vers Google Sheets !"
 
 if __name__ == '__main__':
-   port = int(os.environ.get('PORT', 5000))
-   app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
